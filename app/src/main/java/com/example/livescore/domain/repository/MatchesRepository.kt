f2e20199f20449bd57349179bf4e630ee5d7570f
kanake10 <10kanake@gmail.com>
2022-09-25 11:23:59 +0300

    repository and usecase for the standings feature

--- app/src/main/java/com/example/livescore/domain/repository/MatchesRepository.kt
+++ app/src/main/java/com/example/livescore/domain/repository/MatchesRepository.kt
@@ -2,11 +2,13 @@ package com.example.livescore.domain.repository
 
 import com.example.livescore.domain.models.DataModel
 import com.example.livescore.domain.models.MatchDetailsModel
+import com.example.livescore.domain.models.StandingsModel
 import com.example.livescore.util.Resource
 import kotlinx.coroutines.flow.Flow
 
 
 interface MatchesRepository {
     fun getMatches(): Flow<Resource<List<DataModel>>>
+    fun getStandings(): Flow<Resource<List<StandingsModel>>>
     fun getMatchDetails(id:String):Flow<Resource<MatchDetailsModel>>
 }
