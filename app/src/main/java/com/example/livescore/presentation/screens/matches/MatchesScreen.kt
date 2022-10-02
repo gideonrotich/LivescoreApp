e6f07e2c5c50c87323a409d63b801854bb97ca9a
Giddyrotich433 <Giddyrotich433@gmail.com>
2022-10-02 21:53:44 +0300

    finished working on standings

--- app/src/main/java/com/example/livescore/presentation/screens/matches/MatchesScreen.kt
+++ app/src/main/java/com/example/livescore/presentation/screens/matches/MatchesScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
