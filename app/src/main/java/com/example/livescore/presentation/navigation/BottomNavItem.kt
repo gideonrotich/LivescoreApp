5e62166f6b228e3cba806004fd70f9e127ff930d
kanake10 <10kanake@gmail.com>
2022-10-10 15:08:32 +0300

    use another icon for the scorers

--- app/src/main/java/com/example/livescore/presentation/navigation/BottomNavItem.kt
+++ app/src/main/java/com/example/livescore/presentation/navigation/BottomNavItem.kt
@@ -5,5 +5,5 @@ import com.example.livescore.R
 sealed class BottomNavItem(var title: String, var icon: Int, var screen_route: String) {
     object Games : BottomNavItem("Games", R.drawable.ic_games, "games")
     object Standings : BottomNavItem("Standings", R.drawable.ic_league, "standings")
-    object Topscorer : BottomNavItem("TopScorers", R.drawable.ic_league, "TopScorers")
+    object Topscorer : BottomNavItem("TopScorers", R.drawable.ic_scorers, "TopScorers")
 }
